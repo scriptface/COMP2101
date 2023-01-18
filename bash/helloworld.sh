38f944542ce436bd094d088abc723d4ee5b847b3
scriptface <113059146+scriptface@users.noreply.github.com>
2023-01-18 15:27:57 -0500

    Update helloworld.sh

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# This is my first script
+# It prints out Hello World!
+# This is for the system setup lab in COMP2101 winter 2023 semester
+
+# print out hello world!
 echo Hello World!
+
+# exit command was added to demonstrate pulling changes from github using the git pull command
 exit
